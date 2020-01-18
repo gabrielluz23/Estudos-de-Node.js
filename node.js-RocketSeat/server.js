b776200852382b1d0d945aa2071e90f7527e481f
gabriel yago <gabriellok1212@gmail.com>
2020-01-18 16:01:09 -0300

    node js rocketSeat

--- node.js-RocketSeat/server.js
+++ node.js-RocketSeat/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
+
 //inciando o app
 const app = express();
 
 //permite enviar requisição no  formato de json
 app.use(express.json());
-
+app.use(cors());
 //conexao mongo db
 mongoose.connect('mongodb://localhost:27017/nodeAPI',
  {useNewUrlParser:true,useUnifiedTopology: true  })
